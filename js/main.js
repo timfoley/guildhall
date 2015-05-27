732d0c837f007122f7a01544714579efc5596ace
Tim Foley <thetimfoley@gmail.com>
2015-05-26 20:54:35 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1ZBKNxsXDaNGrP63jYHISp3ckbsxvV0SUwD8VNbllVC4/pubhtml';
+
+var tabletop = Tabletop.init({
+  key: "https://docs.google.com/spreadsheets/d/1ZBKNxsXDaNGrP63jYHISp3ckbsxvV0SUwD8VNbllVC4/pubhtml",
+  callback: sheetLoad,
+  simpleSheet: false
+})
+
+
+
+function sheetLoad( data, tabletop ){
+  render( data )
+  console.log( data )
+  console.log('something')
+}
+
+
+function render( context ){
+  var template = $("#guilds-template").html()
+  var compile = Handlebars.compile( template )
+  var html = compile( { guild: context } )
+  $(".guild-profiles").html( html ).show()
+}
+
+
+render(tabletop.data().Details.all());
