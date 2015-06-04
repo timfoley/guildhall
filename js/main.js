640d92e747e7c4c63f9c58bbc48549a9b792eead
Tim Foley <thetimfoley@gmail.com>
2015-06-04 14:12:46 -0400

    added next meeting

--- js/main.js
+++ js/main.js
@@ -11,9 +11,17 @@ var tabletop = Tabletop.init({
 
 function sheetLoad( data, tabletop ){
   console.log( data )
+  var guildHall = data.Details.all();
+  var js2 = data.js2meetings.all();
+  var dat = data.datMeetings.all();
+  guildHall[1].meetings = js2;
+  guildHall[2].meetings = dat;
+
+  //guildHall[0].meetings = data.js1meetings.all()
+
   console.log(tabletop.data())
   console.log('something')
-  render(data.Details.all())
+  render(guildHall)
 }
 
 
@@ -22,7 +30,19 @@ function render( context ){
   var compile = Handlebars.compile( template )
   var html = compile( { guild: context } )
   $(".guild-profiles").html( html ).show()
+
+  var i = 0;
+  while (i < context.length) {
+    if (context[i].meetings != undefined) {
+      var nextDate = context[i].meetings[0].date;
+      var nextDateLink = context[i].meetings[0].url;
+      $('.'+context[i].id+'-meeting').html("<strong>Next Meeting: </strong><a href=" + nextDateLink + ">" + nextDate);
+    } else {
+    }
+    i++
+  }
 }
+
 
 
 
